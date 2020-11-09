401c48b8e8e84411a349b89a7314b9707c1c1e7c
cymard <cyriansimard@gmail.com>
2020-11-09 09:32:52 +0100

    Form de Register controlle avec Yup et Formik

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container,Form,Button  } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import RegisterForm from './RegisterForm.jsx'
 
 function Compte(){
     return <Container className="d-flex justify-content-center">
@@ -7,25 +8,7 @@ function Compte(){
             <div className="d-flex justify-content-center mb-3 mt-4">
                 <h1>Creer votre compte : </h1>
             </div>
-            
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Adresse Email</Form.Label>
-                    <Form.Control type="email" placeholder="Entrez votre adresse email ..." />
-                    <Form.Text className="text-muted">
-                        Nous ne communiquerons pas votre email
-                    </Form.Text>
-                </Form.Group>
-
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Mot de Passe</Form.Label>
-                    <Form.Control type="password" placeholder="Entrez votre mot de passe ..." />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Valider
-                </Button>
-            </Form>
+            <RegisterForm></RegisterForm>
         </div>
     </Container>
 }
