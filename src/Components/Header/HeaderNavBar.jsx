4a57a0b959917a0e9c0241d8a2377b12dfd9c2cf
cymard <cyriansimard@gmail.com>
2020-11-10 08:11:22 +0100

    Mise en place du menu sous forme de burger pour le responsive.

--- src/Components/Header/HeaderNavBar.jsx
+++ src/Components/Header/HeaderNavBar.jsx
@@ -9,31 +9,30 @@ function HeaderNavBar () {
   const shoppingCartIcon = <FontAwesomeIcon icon={faShoppingCart} />
 
     return <> 
-    <Navbar fixed="top" bg="light" expand="lg" > 
+      <Navbar collapseOnSelect fixed="top" bg="light" expand="lg" > 
 
         <Navbar.Brand>
           <ModifiedLinks color="black" to="/">LOGO</ModifiedLinks>
         </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav">
+
           <Form style={{width: "100%"}} className="d-flex flex-nowrap">
             <FormControl type="text" placeholder="Rechercher" className="mr-sm-2" />
             <Button variant="outline-success">{searchIcon}</Button>
           </Form>
           
           <Nav className="mr-auto">
-               
             <ModifiedLinks color="black" to="/RegisterOrConnection">Compte</ModifiedLinks>
-
             <ModifiedLinks color="black" to="/ShoppingCart">Panier {shoppingCartIcon}</ModifiedLinks>
-
           </Nav>  
+
         </Navbar.Collapse>
+
       </Navbar>
 
-    <div style={{height : "64px"}}>
-
-    </div>
+      <div style={{height : "64px"}}></div>
   </>
 }
 
