a5d6cf0a636bd96f575c93a7eba02e222d2b0649
cymard <cyriansimard@gmail.com>
2021-01-18 10:15:03 +0100

    Arrondissement de la valeur du nombre de page, qui permet maintenant de voir tous les produits.

--- src/Components/ReturnPaginationButtons.jsx
+++ src/Components/ReturnPaginationButtons.jsx
@@ -3,10 +3,9 @@ import {Pagination} from 'react-bootstrap';
 
 function ReturnPaginationButtons ({number, handleClick}) {
  
-        console.log('render pagination')
-
     const test = useCallback((number, handleClick) => {
-        let pageNumber = number/9;
+        let pageNumber = Math.ceil(number/9);
+        console.log(pageNumber);
         let paginationArray = []
         for(let i = 1;i<=pageNumber; i++){
             paginationArray.push(<Pagination.Item onClick={handleClick}>{i}</Pagination.Item>)
